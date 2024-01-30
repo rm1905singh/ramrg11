29e347f024ee8968955e35db12bb3d61ebb641eb
Am <ramsingh26@gmail.com>
2024-01-30 21:18:46 +0530

    1st commit

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 resource "azurerm_resource_group" "rgblock" {
   name     = "rm-rg1"
   location = "West Europe"
+
+}
+resource "azurerm_resource_group" "rgblock1" {
+  name     = "rm-rg2"
+  location = "Westus"
+
 }
